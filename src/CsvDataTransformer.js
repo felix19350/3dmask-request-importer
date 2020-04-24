1e622e2fffffd12f26435831be9c6dba8e178539
José Coelho <josecoelho96@gmail.com>
2020-04-24 23:44:13 +0100

    changes on csvDataTransformer according to the new db schema

--- src/CsvDataTransformer.js
+++ src/CsvDataTransformer.js
@@ -30,11 +30,13 @@ class CsvDataTransformer {
                 .on("data", (row) => {
                     try {
                         n++;
-                        const statementParams = this.transformRow(row);
-                        const columns = statementParams.map(it => it.column).join(",");
-                        const values = statementParams.map(it => this.escapeToSQL(it)).join(",");
-                        const statement = `insert into shield_request (${columns}) values (${values});`;
-                        statements.push(statement);
+                        const transformedRow = this.transformRow(row);
+                        const rowN = `------ ROW: ${n}`;
+                        statements.push(rowN);
+                        const requestTimestamp = `------ ${transformedRow.timestamp}`;
+                        statements.push(requestTimestamp);
+                        statements.push(this.mapRowToSqlStatement(transformedRow.request, 'request'));
+                        this.mapRowToSqlStatements(transformedRow.items, 'request_item').forEach(l => statements.push(l));
                     } catch (err) {
                         errors.push(`ROW: ${n} | Error: ${err.message}`)
                     }
@@ -52,7 +54,19 @@ class CsvDataTransformer {
         });
     }
 
+    mapRowToSqlStatement(inputValues, tableName) {
+        const columns = inputValues.map(it => it.column).join(",");
+        const values = inputValues.map(it => this.escapeToSQL(it)).join(",");
+        const statement = `insert into ${tableName} (${columns}) values (${values});`;
+        return statement;
+    }
+
+    mapRowToSqlStatements(inputValues, tableName) {
+        return inputValues.map(v => this.mapRowToSqlStatement(v, tableName));
+    }
+
     lookupZone(inputZone) {
+        if (inputZone === "") return "";
         const normalizedZone = this.normalizeZoneName(inputZone);
         const result = this.zones.find(z => z.zone === normalizedZone);
         if(!result){
@@ -94,34 +108,104 @@ class CsvDataTransformer {
         return municipalityName.toLowerCase().trim();
     }
 
+    validateStringNotEmpty(row, field) {
+        if (row[field].length === 0) {
+            throw Error(`Field '${field}' cannot be empty!`);
+        }
+        return row[field].trim();
+    }
+
+    validatePostalCode(row, field) {
+        const postalCode = this.validateStringNotEmpty(row, field);
+        if (!/^[0-9]{4}-[0-9]{3}/.test(postalCode)) {
+            throw Error(`Field '${field}' has invalid postal code format! (value: ${postalCode}')`);
+        }
+        return postalCode.match(/^[0-9]{4}-[0-9]{3}/);
+    }
+
+    validateEmailAddress(row, field) {
+        const email = this.validateStringNotEmpty(row, field);
+        if (!/^^\S+@\S+$/.test(email)) {
+            throw Error(`Field '${field}' has invalid email format! (value: '${email}')`);
+        }
+        return email;
+    }
+
+    validatePhoneNumber(row, field) {
+        const phoneNumber = this.validateStringNotEmpty(row, field);
+        if (!/^\+{0,1}[0-9]{9,}/.test(phoneNumber)) {
+            throw Error(`Field '${field}' has invalid phone number format! (value: '${phoneNumber}')`);
+        }
+        return phoneNumber;
+    }
+
     transformRow(row) {
-        return [
-            { column: "id", type: "String", value: uuid.v4() },
-            { column: "entity", type: "String", value: row["Unidade de saúde"] },
-            { column: "service", type: "String", value: row["Serviço"] },
-            { column: "district_id", type: "String", value: this.lookupDistrict(row["Distrito / Região"]) },
-            { column: "municipality_id", type: "String", value: this.lookupMunicipality(row["Concelho"]) },
-            { column: "address", type: "String", value: row["Morada"] },
-            { column: "postal_code", type: "String", value: row["Código Postal"] },
-            { column: "requester_name", type: "String", value: row['Nome '] }, // do not remove the extra space in the row name
-            { column: "requester_email", type: "String", value: row['Email'] },
-            { column: "requester_phone_number", type: "String", value: row['Telemóvel'] },
-            { column: "quantity", type: "Number", value: row['Quantidade'] },
-            { column: "observations", type: "String", value: row['Observações'] },
-            { column: "priority", type: "String", value: this.lookupPriority(row['Prioridade (Manual)']) },
-            { column: "status", type: "String", value: this.lookupStatus(row['Estado (Manual)']) },
-            { column: "zone_id", type: "String", value: this.lookupZone(row["Zona (Manual)"]) },
-            { column: "normalized_entity_name", type: "String", value: row["Nome da Unidade de Saúde (Normalizado)"] },
-            { column: "when_created", type: "String", value: this.parseDate(row['Timestamp']) },
-            { column: "when_modified", type: "String", value: this.parseDate(row['Timestamp']) },
-            { column: "who_created", type: "String", value: "system" },
-            { column: "who_modified", type: "String", value: "system" },
-        ]
+        const requestId = uuid.v4();
+        const createTimestamp = this.parseDate(row['Timestamp']);
+        const items = [];
+
+        if (row["Quantidade de Viseiras"] !== "" && row["Quantidade de Viseiras"] > 0) {
+            items.push([
+                { column: "id", type: "String", value: uuid.v4() },
+                { column: "request_id", type: "String", value: requestId },
+                { column: "product_id", type: "String", value: "2c46263c-fb78-47e4-9502-44f1a532f7c5" }, // Product Id of "viseiras"
+                { column: "quantity", type: "Number", value: row["Quantidade de Viseiras"] },
+                { column: "observations", String: "NullableString", value: "null" },
+                { column: "when_created", type: "String", value: createTimestamp },
+                { column: "when_modified", type: "String", value: createTimestamp },
+                { column: "who_created", type: "String", value: "anonymous" },
+                { column: "who_modified", type: "String", value: "anonymous" },
+            ]);
+        }
+
+        if (row["Quantidade de \"salva-orelhas\""] !== "" && row["Quantidade de \"salva-orelhas\""] > 0) {
+            items.push([
+                { column: "id", type: "String", value: uuid.v4() },
+                { column: "request_id", type: "String", value: requestId },
+                { column: "product_id", type: "String", value: "d1c41108-1b18-4939-8f00-69855e74363b" }, // Product Id of "salva-orelhas"
+                { column: "quantity", type: "Number", value: row["Quantidade de \"salva-orelhas\""] },
+                { column: "observations", String: "NullableString", value: "null" },
+                { column: "when_created", type: "String", value: createTimestamp },
+                { column: "when_modified", type: "String", value: createTimestamp },
+                { column: "who_created", type: "String", value: "anonymous" },
+                { column: "who_modified", type: "String", value: "anonymous" },
+            ]);
+        }
+
+        return {
+            timestamp: row['Timestamp'],
+            request: [
+                { column: "id", type: "String", value: requestId },
+                { column: "institution_type_id", type: "String", value: "bceeed6d-9a71-402c-9f81-f7747d63aca2" }, // TODO Fallback
+                { column: "institution", type: "String", value: this.validateStringNotEmpty(row, "Instituição") },
+                { column: "normalized_institution_name", type: "NullableString", value: row["Nome da Unidade de Saúde (Normalizado)"] },
+                { column: "service", type: "String", value: this.validateStringNotEmpty(row, "Serviço Prestado") },
+                { column: "district_id", type: "String", value: this.lookupDistrict(row["Distrito / Região"]) },
+                { column: "municipality_id", type: "String", value: this.lookupMunicipality(row["Concelho"]) },
+                { column: "address", type: "String", value: this.validateStringNotEmpty(row, "Morada") },
+                { column: "postal_code", type: "String", value: this.validatePostalCode(row, "Código Postal") },
+                { column: "zone_id", type: "NullableString", value: this.lookupZone(row["Zona (Manual)"]) },
+                { column: "requester_name", type: "String", value: this.validateStringNotEmpty(row, 'Nome ') }, // do not remove the extra space in the row name
+                { column: "requester_email", type: "String", value: this.validateEmailAddress(row, 'Email') },
+                { column: "requester_phone_number", type: "String", value: this.validatePhoneNumber(row, 'Telemóvel') },
+                { column: "observations", type: "NullableString", value: row['Observações'] },
+                { column: "priority", type: "String", value: this.lookupPriority(row['Prioridade (Manual)']) },
+                { column: "status", type: "String", value: this.lookupStatus(row['Estado (Manual)']) },
+                { column: "when_created", type: "String", value: createTimestamp },
+                { column: "when_modified", type: "String", value: createTimestamp },
+                { column: "who_created", type: "String", value: "anonymous" },
+                { column: "who_modified", type: "String", value: "anonymous" },
+                // { column: "claimant", type: "null", value: "null" },
+            ],
+            items: items
+        }
     }
 
     escapeToSQL(entry) {
         if (entry.type === "String") {
             return `'${entry.value ? entry.value : ""}'`;
+        } else if (entry.type === "NullableString") {
+            return entry.value ? `'${entry.value}'` : "null";
         } else {
             return entry.value ? entry.value : "";
         }
